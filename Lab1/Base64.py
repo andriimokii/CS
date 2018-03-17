d09942a2752889bc104febbf029f1cf3a2ee7366
AndriiMokii <andriimokii@MacBook-Air-AndriiMokii.local>
2018-03-17 20:38:45 +0200

    Lab_1_CS

---
+++ Lab1/Base64.py
@@ -0,0 +1,29 @@
+from bitstring import BitArray
+
+try:
+    with open('/Users/andriimokii/Documents/CS/Lab1/text/archive-3/CS-Lab1-text2.txt.bz2', 'rb') as f, open('/Users/andriimokii/Documents/CS/Lab1/text/archive-3/BASE64_arch.txt', 'wt', encoding='UTF-8') as out:
+        pattern = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+        buff_b1 = [ ] 
+        counter = -1
+        num = 0
+        for i in BitArray(f.read()).cut(8):
+            counter+=1
+            if counter == 0:
+                out.write(pattern[i[ : 6].int])
+            elif counter == 1:
+                out.write(pattern[int(buff_b1[0][6:] + str(i[:4].bin), 2)])
+            else:
+                out.write(pattern[int(buff_b1[1][4:] + str(i[:2].bin), 2)])                    
+                out.write(pattern[i[2:].int])
+            buff_b1.append(str(i.bin))
+            if counter == 2:
+                del buff_b1[:]
+                counter = -1
+        if counter == 0:
+            out.write(pattern[int(buff_b1[0][6:] + '0000', 2)] + '==')
+        elif counter == 1:
+            out.write(pattern[int(buff_b1[1][4:] + '00', 2)] + '=')
+except Exception as ex:
+    print("Error:", ex)
+
+
---
+++ Lab1/CS-Lab1-1.py
@@ -0,0 +1,32 @@
+import os
+from math import log
+try:
+    alph = { }
+    summ = 0
+    FilePath = '/Users/andriimokii/Documents/CS/Lab1/text/archive-1/BASE64.txt'
+    stream = open(FilePath, 'rt', encoding='UTF-8')
+    Text = stream.read()
+    for ch in Text:
+        #if ch.isalpha():
+            if ch in alph.keys():
+                alph[ch]+=1
+            else:
+                alph[ch]=1
+    #text_cnt = open('/Users/andriimokii/Documents/text/CS-TextCounter.txt', 'wt', encoding='UTF-8')
+    for key in sorted(alph.keys()):
+        summ+=alph[key]
+        print(key, '->', alph[key])
+    for el in sorted(alph.keys()):
+        print('Probability:', el, '->', alph[el] / summ)
+    m = 1
+    H = 0
+    for key in sorted(alph.keys()):
+        H += (alph[key] / summ) * log(1/(alph[key] / summ),2)
+    print('Entropii:', H)
+    print('Amount of info:', (summ * H) / 8 )
+    print('Real size:', os.stat(FilePath).st_size)
+    print('amount of letters',summ)       
+    stream.close()
+except Exception as ex:
+    print("error is:", os.strerror(ex.errno))
+    
