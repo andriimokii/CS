aac3d38f970707c57f22812b871c4b49385256bc
Andrii Mokii <powerexpexp@gmail.com>
2018-04-06 10:32:41 +0300

    back

--- Lab2/Lab1.py
+++ Lab2/Lab1.py
@@ -1,37 +1,44 @@
 #!/usr/local/bin/python3
 from bitstring import BitArray
+
+def NOP_00_11()
+#Interation counter and Size of registers
 count = 0
+REGSIZE = 5
+
 
 #Multiplicand and Multiplier
-M = BitArray(int=-20, length=11)
-Q = BitArray(int=7, length=11)
-print(Q.bin, '*', M.bin, '(', Q.int, '*', M.int, ')')
-#initialization
-A = BitArray('0b' + '0' * M.length + str(Q.bin) + '0')
+Multiplicand = BitArray(int=15, length=REGSIZE)
+Multiplier = BitArray(int=-4, length=REGSIZE)
+print(Multiplicand.bin, '*', Multiplier.bin, '(', Multiplicand.int, '*', Multiplier.int, ')')
 
-while count < M.length:
-    if str(A[2 * M.length - 1:].bin) == '00' or str(A[2*M.length - 1:].bin) == '11' :
-        print('Register:', A.bin, '--> 00 or 11-NOP -->', end=' ')
-        A.int >>= 1
-        print('shift right -->',A.bin)
-    elif str(A[2*M.length - 1:].bin) == '01' :
-        print('Register:', A.bin, '--> 01-ADD -->', end=' ')
-        lol = BitArray(int=A[:M.length].int + M.int, length = M.length + 1)
-        del lol[0:1]
-        A.overwrite(lol, 0)
-        print('ADDED:', A.bin, '-->', end=' ')
-        A.int >>= 1
-        print('shift right -->',A.bin)
+#initialization
+Line = BitArray('0b' + '0' * Multiplicand.length + str(Multiplier.bin) + '0')
+print('Initialization:', Line.bin)
+
+while count < Multiplicand.length:
+    if str(Line[2 * Multiplicand.length - 1:].bin) == '00' or str(Line[2*Multiplicand.length - 1:].bin) == '11' :
+        print('Register:', Line.bin, '--> 00 or 11-NOP -->', end=' ')
+        Line.int >>= 1
+        print('shift right -->',Line.bin)
+    elif str(Line[2*Multiplicand.length - 1:].bin) == '01' :
+        print('Register:', Line.bin, '--> 01-ADD -->', end=' ')
+        extra_bit = BitArray(int=Line[:Multiplicand.length].int + Multiplicand.int, length = Multiplicand.length + 1)
+        del extra_bit[0:1]
+        Line.overwrite(extra_bit, 0)
+        print('ADDED:', Line.bin, '-->', end=' ')
+        Line.int >>= 1
+        print('shift right -->',Line.bin)
     else:
-        print('Register:', A.bin, '--> 10-SUB -->', end=' ')
-        heh = BitArray(int=A[:M.length].int - M.int, length = M.length + 1)
-        del heh[0:1]
-        A.overwrite(heh, 0)
-        print('SUBTRACTED:', A.bin, '-->', end= ' ')
-        A.int >>= 1
-        print('shift right -->', A.bin)
+        print('Register:', Line.bin, '--> 10-SUB -->', end=' ')
+        extra_bit = BitArray(int=Line[:Multiplicand.length].int - Multiplicand.int, length = Multiplicand.length + 1)
+        del extra_bit[0:1]
+        Line.overwrite(extra_bit, 0)
+        print('SUBTRACTED:', Line.bin, '-->', end= ' ')
+        Line.int >>= 1
+        print('shift right -->', Line.bin)
     count+=1
-del A[-1]
-print('Binary register: ', A.bin)
-print('Decimal register: ', A.int)
+del Line[-1]
+print('Binary register: ', Line.bin)
+print('Decimal register: ', Line.int)
 
